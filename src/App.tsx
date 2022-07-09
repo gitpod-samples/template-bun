c236a4716d2b0c1c112933d6df40149adc460915
Joshuafrankle <joshuafrankle7@gmail.com>
2022-07-09 08:23:55 +0000

    update files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
